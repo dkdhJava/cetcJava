2ceb6ae7f287657dcea2bd704813818e16f9a014
cetcJava <cetc_java@126.com>
2014-08-18 20:30:21 +0800

    多打印好几行

--- MyCXFTest/src/com/guohao/cxf/HelloWorldImpl.java
+++ MyCXFTest/src/com/guohao/cxf/HelloWorldImpl.java
@@ -5,6 +5,9 @@ public class HelloWorldImpl implements HelloWorld {
 	public void hello(String name) {
 		System.out.println("CXF Test name=" + name);
 		System.out.println("CXF Test name=" + name);
+		System.out.println("CXF Test name=" + name);
+		System.out.println("CXF Test name=" + name);
+		System.out.println("CXF Test name=" + name);
 	}
 
 }
