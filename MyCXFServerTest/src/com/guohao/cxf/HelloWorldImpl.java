821cdd64207fd8466cff680e03001a6adbde011e
cetcJava <cetc_java@126.com>
2014-08-19 15:41:10 +0800

    打印两行

--- MyCXFServerTest/src/com/guohao/cxf/HelloWorldImpl.java
+++ MyCXFServerTest/src/com/guohao/cxf/HelloWorldImpl.java
@@ -3,6 +3,8 @@ package com.guohao.cxf;
 public class HelloWorldImpl implements IHelloWorld {
 	public String hello(String name) {
 		System.out.println("hello," + name);
+		System.out.println("hello," + name);
+		System.out.println("hello," + name);
 		return "hello," + name;
 	}
 
