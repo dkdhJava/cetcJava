b97e82e11de8c113c17ef1d5392dba8d3e4d34c2
dkdhJava <cetc_java@126.com>
2014-08-22 16:30:50 +0800

    1 2 a

--- MyCXFServerTest/src/com/guohao/cxf/HelloWorldImpl.java
+++ MyCXFServerTest/src/com/guohao/cxf/HelloWorldImpl.java
@@ -3,6 +3,7 @@ package com.guohao.cxf;
 public class HelloWorldImpl implements IHelloWorld {
 	public String hello(String name) {
 		System.out.println("1");
+		System.out.println("2");
 		System.out.println("a");
 		return "hello," + name;
 	}
