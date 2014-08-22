43aa8252384ebbf7cbb96e93ee3e12ad4c1bd3cd
dkdhJava <cetc_java@126.com>
2014-08-22 10:22:50 +0800

    new starter

--- MyCXFServerTest/src/com/guohao/cxf/HelloWorldImpl.java
+++ MyCXFServerTest/src/com/guohao/cxf/HelloWorldImpl.java
@@ -2,24 +2,8 @@ package com.guohao.cxf;
 
 public class HelloWorldImpl implements IHelloWorld {
 	public String hello(String name) {
-		System.out.println("hello," + name);
-		System.out.println("hello," + name);
-		System.out.println("hello," + name);
-		System.out.println("hello," + name);
-		System.out.println("hello," + name);
-		System.out.println("hello," + name);
-		System.out.println("hello,55555555555555" + name);
-		System.out.println("hello,55555555555555" + name);
-		System.out.println("hello,55555555555555" + name);
-		System.out.println("hello,55555555555555" + name);
-		System.out.println("hello," + name);
-		System.out.println("hello," + name);
-		System.out.println("hello," + name);
-		System.out.println("hello," + name);
-		System.out.println("hello," + name);
-		System.out.println("hello," + name);
-		System.out.println("hello,the world" );
-		System.out.println("hello,guohao" );
+		System.out.println("hello,1");
+		System.out.println("hello,a");
 		return "hello," + name;
 	}
 
